b5eeb35dbbd3e7c91119df022eebfdf405300646
shybyte <shybyte@users.noreply.github.com>
2023-09-11 09:18:38 +0200

    Real ADSR

--- src/constants.zig
+++ src/constants.zig
@@ -1,4 +1,5 @@
 pub const SAMPLE_RATE = 44100;
+pub const SAMPLE_RATE_F32: f32 = SAMPLE_RATE;
 pub const ENABLE_MIDI = true;
 pub const ENABLE_FILES = true;
 pub const MIDI_INPUT_DEVICE_NUMBER = 3;
--- src/synth/envelops.zig
+++ src/synth/envelops.zig
@@ -1,16 +1,35 @@
+const SAMPLE_RATE = @import("../constants.zig").SAMPLE_RATE_F32;
+
 pub const Adsr = struct {
-    value: f32 = 0,
+    time: f32 = 0,
+    attack_time: f32 = 0.01,
+    decay_time: f32 = 0.5,
+    sustain_level: f32 = 0.5,
+    release_time: f32 = 0.5,
 
     const Self = @This();
 
     pub fn trigger(self: *@This()) void {
-        self.value = 1.0;
+        self.time = 0.0;
     }
 
     pub fn generate(self: *@This()) f32 {
-        if (self.value > 0) {
-            self.value = self.value - 0.00006;
+        self.time += 1.0 / SAMPLE_RATE;
+
+        if (self.time <= self.attack_time) {
+            return self.time / self.attack_time;
         }
-        return self.value;
+
+        const time_in_decay = self.time - (self.attack_time);
+        if (time_in_decay <= self.decay_time) {
+            return 1 - time_in_decay / self.decay_time * (1 - self.sustain_level);
+        }
+
+        const time_in_release = self.time - (self.attack_time + self.decay_time);
+        if (time_in_release <= self.release_time) {
+            return self.sustain_level - time_in_release / self.release_time * self.sustain_level;
+        }
+
+        return 0;
     }
 };
